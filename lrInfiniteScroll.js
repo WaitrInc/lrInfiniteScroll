a0f5d1118df30e4998b9c2d21da08d1b8c6dcd32
James Mallory <james.mallory@waitrapp.com>
2017-05-05 15:42:48 -0500

    feat: add top scroll event checker

--- lrInfiniteScroll.js
+++ lrInfiniteScroll.js
@@ -7,10 +7,13 @@
             link: function (scope, element, attr) {
                 var
                     lengthThreshold = attr.scrollThreshold || 50,
+                    topLengthThreshold = attr.topScrollThreshold || 50,
                     timeThreshold = attr.timeThreshold || 400,
                     handler = scope.$eval(attr.lrInfiniteScroll),
+                    handlerTop = scope.$eval(attr.lrInfiniteScrollTop),
                     promise = null,
-                    lastRemaining = 9999;
+                    lastRemaining = 9999,
+                    lastTop = 0;
 
                 lengthThreshold = parseInt(lengthThreshold, 10);
                 timeThreshold = parseInt(timeThreshold, 10);
@@ -19,9 +22,14 @@
                     handler = ng.noop;
                 }
 
+                if (!handlerTop || !ng.isFunction(handlerTop)) {
+                    handlerTop = ng.noop;
+                }
+
                 element.bind('scroll', function () {
                     var
-                        remaining = element[0].scrollHeight - (element[0].clientHeight + element[0].scrollTop);
+                        remaining = element[0].scrollHeight - (element[0].clientHeight + element[0].scrollTop),
+                        top = element[0].scrollTop;
 
                     //if we have reached the threshold and we scroll down
                     if (remaining < lengthThreshold && (remaining - lastRemaining) < 0) {
@@ -35,7 +43,21 @@
                             promise = null;
                         }, timeThreshold);
                     }
+
+                    //if we have reached the threshold and we scroll up
+                    if (top < topLengthThreshold && top < lastTop){
+                        //if there is already a timer running which has no expired yet we have to cancel it and restart the timer
+                        if (promise !== null) {
+                            timeout.cancel(promise);
+                        }
+                        promise = timeout(function () {
+                            handlerTop();
+                            promise = null;
+                        }, timeThreshold);
+                    }
+
                     lastRemaining = remaining;
+                    lastTop = top;
                 });
             }
 
